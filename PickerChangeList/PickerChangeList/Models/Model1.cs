e703e24d167718d62f0bd89a116666ee168b2c04
Kazuki Ota <kaota@microsoft.com>
2020-03-14 18:20:53 +0900

    コンストラクターで初期化するものは private set はいらない

--- PickerChangeList/PickerChangeList/Models/Model1.cs
+++ PickerChangeList/PickerChangeList/Models/Model1.cs
@@ -13,7 +13,7 @@ namespace PickerChangeList.Models {
         public string Name { get => name; set => SetProperty(ref name, value); }
         private string name;
 
-        public ObservableCollection<Model2> Model2s { get; private set; }
+        public ObservableCollection<Model2> Model2s { get; }
 
         public Model1(List<Model2> m) => Model2s = new ObservableCollection<Model2>(m);
     }
